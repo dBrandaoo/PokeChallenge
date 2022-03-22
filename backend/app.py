5793bc940d376be4b4d3020179ddd02b8e9a2577
Diogo Brandão <dbrandao7@ua.pt>
2022-03-22 23:10:25 +0000

    ADDED endpoints

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,6 @@
 from flask import Flask
+from settings.database import DB
+import json
 
 app = Flask(__name__)
 
@@ -6,3 +8,15 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     return '<h1>Hello, World!</h1>'
+
+
+@app.route('/pokemon/', methods=['GET'])
+def pokemon():
+    cursor = DB.cursor()
+
+    cursor.execute("SELECT * FROM pokemon WHERE ID=%s", (1,))
+    return json.dumps(cursor.fetchall()[0])
+
+
+if __name__ == '__main__':
+    app.run()
